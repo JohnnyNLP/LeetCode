1961acf7e3e20f0916e0c9f5f3e2dcd09298cd98
Chanhee Park <46876504+JohnnyNLP@users.noreply.github.com>
2024-09-03 14:38:05 +0900

    Time: 30 ms (60.79%), Space: 13.6 MB (40.60%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -7,11 +7,7 @@ class Solution(object):
         :rtype: bool
         """
         text_only = re.sub("[^0-9a-zA-Z]", "", s).lower()
-        mid_point = len(text_only)//2
 
-        left_half = text_only[:mid_point]
-        right_half = ''.join(reversed(text_only))[:mid_point]
-
-        if left_half == right_half :
+        if text_only == text_only[::-1] :
             return True
         else : return False
