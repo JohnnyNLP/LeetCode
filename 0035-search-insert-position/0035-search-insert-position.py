7964e74d55eb38da234be61fb7fd5aec27cc2b9f
Chanhee Park <46876504+JohnnyNLP@users.noreply.github.com>
2024-09-04 22:12:55 +0900

    Time: 28 ms (64.49%), Space: 12.4 MB (54.57%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) # could be [0, n], [1, n] etc. Depends on problem
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] >= target:
+                right = mid
+            else:
+                left = mid + 1
+        return left
