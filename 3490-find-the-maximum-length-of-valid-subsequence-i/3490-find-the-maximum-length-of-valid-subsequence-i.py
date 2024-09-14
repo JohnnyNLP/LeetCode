8eda2ccb1b4419b1d4225ffdca82315b18c8e993
Chanhee Park <46876504+JohnnyNLP@users.noreply.github.com>
2024-09-14 21:21:42 +0900

    Time: 1262 ms (5.55%), Space: 29.1 MB (68.52%) - LeetHub

--- 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.py
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.py
@@ -14,20 +14,17 @@ class Solution(object):
         else : o_count += 1
 
         for idx, num in enumerate(nums): 
-            # check for even-odd alt
-            if expect_odd and num % 2 == 1:
+            # check for alt count
+            if (expect_odd and num % 2 == 1) or (not expect_odd and num % 2 == 0):
                 alt_count += 1
-                expect_odd = False
-            # check for odd-even alt
-            elif not expect_odd and num % 2 == 0:
-                alt_count += 1
-                expect_odd = True
-            
+                expect_odd = not expect_odd
+
             if idx > 0 and num % 2 == 0:
                 e_count += 1
             elif idx > 0 and num % 2 == 1:
                 o_count += 1
         
+        print(alt_count, o_count, e_count)
         return max(alt_count, o_count, e_count)
 
                 
