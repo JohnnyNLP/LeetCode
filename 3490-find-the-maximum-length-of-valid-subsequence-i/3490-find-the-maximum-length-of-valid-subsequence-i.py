d5c6e17b2ffeb25c05a7959e29e2117bd8db925b
Chanhee Park <46876504+JohnnyNLP@users.noreply.github.com>
2024-09-14 21:14:37 +0900

    Time: 1305 ms (5.55%), Space: 29.2 MB (68.52%) - LeetHub

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/3490-find-the-maximum-length-of-valid-subsequence-i.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def maximumLength(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        if len(nums) == 2 : return 2
+
+        alt_count, o_count, e_count = 1, 0, 0
+        # initial case setup
+        expect_odd = (nums[0]%2 == 0)
+        if expect_odd : e_count += 1
+        else : o_count += 1
+
+        for idx, num in enumerate(nums): 
+            # check for even-odd alt
+            if expect_odd and num % 2 == 1:
+                alt_count += 1
+                expect_odd = False
+            # check for odd-even alt
+            elif not expect_odd and num % 2 == 0:
+                alt_count += 1
+                expect_odd = True
+            
+            if idx > 0 and num % 2 == 0:
+                e_count += 1
+            elif idx > 0 and num % 2 == 1:
+                o_count += 1
+        
+        print(alt_count, o_count, e_count)
+        return max(alt_count, o_count, e_count)
+
+                
